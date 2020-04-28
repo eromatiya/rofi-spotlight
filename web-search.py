d9e621979b51ecf24cd55bd79b8e6e459b818a12
Gerome Matilla <gerome.matilla07@gmail.com>
2020-04-28 10:03:02 +0800

    fix web search bug

--- web-search.py
+++ web-search.py
@@ -154,7 +154,7 @@ def main():
         search_string = search_string.rstrip('!').strip()
         results = fetch_suggestions(search_string)
         for r in results:
-            print(html.unescape(r) + "\0icon\x1f"+icon_name+"\n")
+            print(":wb " + html.unescape(r) + "\0icon\x1f"+icon_name+"\n")
     else:
         url = CONFIG['SEARCH_URL'][SEARCH_ENGINE] + urllib.parse.quote_plus(search_string)
         sp.Popen(CONFIG['BROWSER_PATH'][BROWSER] + [url], stdout=sp.DEVNULL, stderr=sp.DEVNULL, shell=False)
@@ -162,5 +162,6 @@ def main():
 if __name__ == "__main__":
     try:
         main()
-    except:
-        sys.exit(1)
+    except Exception as e:
+        if e:
+            sys.exit(1)
