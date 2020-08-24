585728540f9de8dab52dcbd78f45ad040630d6ef
AethanFoot <63980259+AethanFoot@users.noreply.github.com>
2020-08-24 22:49:09 +0100

    Replaced tr is xdg function (#8)
    * swap find with fd
    
    * fix small issues
    
    * fix small issues
    
    * update dependencies & changed to use default file manager
    
    * Fixed error showing when doing a ! query with space
    
    * replace tr in return_xdg_dir for faster alternative
    
    * remove printf echo

--- rofi-spotlight.sh
+++ rofi-spotlight.sh
@@ -287,7 +287,7 @@ function icon_file_type(){
 			;;
 	esac
 
-	echo "${1}""\0icon\x1f""${icon_name}""\n"
+	echo -en "$1\0icon\x1f$icon_name\n"
 }
 
 
@@ -424,7 +424,7 @@ function navigate_to() {
 		do
 			if [[ -d "${i}" ]] && ([[ "${i}" != "./" ]] && [[ "${i}" != "../"* ]])
 			then
-				printf "$(icon_file_type "${i}")";
+				icon_file_type "${i}"
 			fi
 		done
 
@@ -432,7 +432,7 @@ function navigate_to() {
 		do 
 			if [[ -f "${i}" ]]
 			then
-				printf "$(icon_file_type "${i}")";
+				icon_file_type "${i}"
 			fi
 		done
 
@@ -442,7 +442,7 @@ function navigate_to() {
 	do 
 		if [[ -d "${i}" ]]
 		then
-			printf "$(icon_file_type "${i}")";
+			icon_file_type "${i}"
 		fi
 	done
 
@@ -450,14 +450,14 @@ function navigate_to() {
 	do 
 		if [[ -f "${i}" ]]
 		then
-			printf "$(icon_file_type "${i}")";
+			icon_file_type "${i}"
 		fi
 	done
 }
 
 # Set XDG dir
 function return_xdg_dir() {
-	target_dir=$(echo "$1" | tr "[:lower:]" "[:upper:]")
+	target_dir=${1^^}
 
 	if [[ "HOME" == *"${target_dir}"* ]]
 	then
